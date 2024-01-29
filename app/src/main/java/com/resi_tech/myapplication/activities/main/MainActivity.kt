84725ff619d08f10554bb0c25968d46809259af2
Emilia Poruszek <emilia.poruszek@geek-about.com>
2024-01-29 20:03:24 +0100

    Small clean up

--- app/src/main/java/com/resi_tech/myapplication/activities/main/MainActivity.kt
+++ app/src/main/java/com/resi_tech/myapplication/activities/main/MainActivity.kt
@@ -4,34 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.resi_tech.myapplication.activities.main.components.ChatItem
 import com.resi_tech.myapplication.components.DarkGrayText
-import com.resi_tech.myapplication.components.ImageWithPlaceholder
-import com.resi_tech.myapplication.components.RoundedCard
-import com.resi_tech.myapplication.models.Author
-import com.resi_tech.myapplication.ui.theme.DimenScheme
 import com.resi_tech.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
